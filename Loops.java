0d53fb7175e681774a7533826239f3dcc8db720a
Awnish Kumar <116937561+AwnishAniSrivastava@users.noreply.github.com>
2022-12-02 11:25:16 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,11 @@
+package CollegeClass;
+
+public class Loops {
+    public static void main(String[] args) {
+        //for loop 
+        //syntex for(start; end; operation){//repeat the code}
+        for(int i=1;i<=10;i++){
+            System.out.println("Awnish");
+        }
+    }
+}
